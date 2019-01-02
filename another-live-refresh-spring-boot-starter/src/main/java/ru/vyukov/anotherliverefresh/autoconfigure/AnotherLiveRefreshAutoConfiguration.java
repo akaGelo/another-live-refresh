3236ddb967f5dbde267e011b2fbfeb0b13ee5db9
Oleg Vyukov <gelo@vyukov.ru>
2019-01-02 20:46:26 +0300

    fix java 9 internal URLClassLoader

--- another-live-refresh-spring-boot-starter/src/main/java/ru/vyukov/anotherliverefresh/autoconfigure/AnotherLiveRefreshAutoConfiguration.java
+++ another-live-refresh-spring-boot-starter/src/main/java/ru/vyukov/anotherliverefresh/autoconfigure/AnotherLiveRefreshAutoConfiguration.java
@@ -1,11 +1,16 @@
 package ru.vyukov.anotherliverefresh.autoconfigure;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -23,9 +28,7 @@ import ru.vyukov.anotherliverefresh.wrap.ResponseWrappedDispatcherServlet;
 import ru.vyukov.anotherliverefresh.ws.LiveRefreshConnectionHandler;
 
 /**
- * 
  * @author gelo
- *
  */
 @Configuration
 @EnableWebSocket
@@ -37,31 +40,47 @@ import ru.vyukov.anotherliverefresh.ws.LiveRefreshConnectionHandler;
 public class AnotherLiveRefreshAutoConfiguration implements WebSocketConfigurer {
 
 
-	@Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
-	public DispatcherServlet dispatcherServlet() {
-		return new ResponseWrappedDispatcherServlet();
-	}
+    @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+    public DispatcherServlet dispatcherServlet() {
+        return new ResponseWrappedDispatcherServlet();
+    }
 
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(liveRefreshwebSocketHandler(), "/alr/refresh");
-	}
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(liveRefreshwebSocketHandler(), "/alr/refresh");
+    }
 
-	@Bean
-	public LiveRefreshConnectionHandler liveRefreshwebSocketHandler() {
-		return new LiveRefreshConnectionHandler();
-	}
+    @Bean
+    public LiveRefreshConnectionHandler liveRefreshwebSocketHandler() {
+        return new LiveRefreshConnectionHandler();
+    }
 
-	@Bean
-	public FileChangeListenerService classPathChangeListenerService(@Autowired AnotherLiveRefreshProperties properties)
-			throws IOException, URISyntaxException {
-		URL[] urls = ((URLClassLoader) (Thread.currentThread().getContextClassLoader())).getURLs();
+    @Bean
+    public FileChangeListenerService classPathChangeListenerService(@Autowired AnotherLiveRefreshProperties properties)
+            throws IOException, URISyntaxException {
+        URL[] urls = urlsFromClassLoader(Thread.currentThread().getContextClassLoader());
 
-		FileChangeListenerServiceImpl fileChangeListenerServiceImpl = new FileChangeListenerServiceImpl(
-				Arrays.asList(urls), properties);
+        FileChangeListenerServiceImpl fileChangeListenerServiceImpl = new FileChangeListenerServiceImpl(
+                Arrays.asList(urls), properties);
 
-		fileChangeListenerServiceImpl.addFileChangeListener(liveRefreshwebSocketHandler());
-		return fileChangeListenerServiceImpl;
-	}
+        fileChangeListenerServiceImpl.addFileChangeListener(liveRefreshwebSocketHandler());
+        return fileChangeListenerServiceImpl;
+    }
+
+
+    private static URL[] urlsFromClassLoader(ClassLoader classLoader) {
+        if (classLoader instanceof URLClassLoader) {
+            return ((URLClassLoader) classLoader).getURLs();
+        }
+        return Stream
+                .of(ManagementFactory.getRuntimeMXBean().getClassPath()
+                        .split(File.pathSeparator))
+                .map(AnotherLiveRefreshAutoConfiguration::toURL).toArray(URL[]::new);
+    }
+
+    @SneakyThrows(MalformedURLException.class)
+    private static URL toURL(String classPathEntry) {
+        return new File(classPathEntry).toURI().toURL();
+    }
 
 }
